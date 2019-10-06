5aab498e880ddfb36c0a15f3d8d74834b2e55e08
Danny Lee <dreamer@panix.com>
2019-10-06 06:46:39 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,42 @@
+class Show < ActiveRecord::Base
+    def self.highest_rating
+        self.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        self.where("rating = ?", self.highest_rating).first
+    end
+
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        self.where("rating = ?", self.lowest_rating).first
+    end
+
+    def self.ratings_sum
+        sum = 0
+        self.all.each do |show|
+            sum += show.rating
+            # binding.pry
+        end
+        sum
+    end
+
+    def self.popular_shows
+        # array = []
+        # self.all.each do |show|
+        #     if show.rating >= 5
+        #         array << show
+        #     end
+        # end
+        # array
+        self.where("rating >= ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
+
+end
